ece6761826c50b5f0f181cad8e12dfbc8393284a
koadegno <vinove9@hotmail.fr>
2023-05-24 15:21:52 +0200

    update densenet

--- train_DenseNet.py
+++ train_DenseNet.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from keras_preprocessing.image import ImageDataGenerator
 from sklearn.model_selection import train_test_split
 from pathlib import Path
 import argparse
@@ -14,154 +15,165 @@ SAVE_DIR = "backup_denseNet_2"  # Save directory for backup weights during the t
 
 
 class SignLanguageClassifier:
-	"""
-	Image classifier for American sign language letter in
- 	form of Picture using Dense Convolutional Network (DenseNet)
-	"""
+    """
+    Image classifier for American sign language letter in
+     form of Picture using Dense Convolutional Network (DenseNet)
+    """
 
-	IMG_HEIGHT = 128
-	IMG_WIDTH = 128
-	IMG_COLOR = 3
-	BATCH_SIZE = 32
-	INPUT_SHAPE = (IMG_HEIGHT,IMG_WIDTH,IMG_COLOR)
-	NUM_CLASSES = 26
+    IMG_HEIGHT = 128
+    IMG_WIDTH = 128
+    IMG_COLOR = 1
+    BATCH_SIZE = 32
+    INPUT_SHAPE = (IMG_HEIGHT,IMG_WIDTH,IMG_COLOR)
+    NUM_CLASSES = 24+1
 
-	def __init__(self, data_train_csv="train_data.csv",data_dir="train", epochs=50):
-		"""
-		:param data_csv_dir: file containing image path and label associated
-		:param data_dir: directory of the data
-		:param epochs: number of epochs for the training
-		"""
-		self.epochs = epochs
-		self.data_dir = data_dir
-		self.data_train = pd.read_csv(data_train_csv)
-		self.data_test = pd.read_csv("test_data.csv")
-		self.data_valid = pd.read_csv("valid_data.csv")
-		self.NUM_CLASSES = len(np.unique(self.data_train['letter']))
 
-		# Load data
-		self.X_train = self._read_csv_data("train",self.data_train)
-		self.X_test = self._read_csv_data('test', self.data_test)
-		self.X_valid = self._read_csv_data('valid', self.data_valid)
+    def __init__(self, data_csv_dir="sign_mnist_train.csv",epochs=5):
+        """
+        :param data_csv_dir: file containing image path and label associated
+        :param data_dir: directory of the data
+        :param epochs: number of epochs for the training
+        """
+        self.epochs = epochs
+        train_df = pd.read_csv(data_csv_dir)
+        self.y_train = train_df['label']
+        del train_df['label']
+        self.x_train = train_df.values
+        self.x_train = self.x_train / 255
+        self.x_train = self.x_train.reshape(-1, 28, 28, 1)
+
+        print(len(np.unique(self.y_train)))
+
+        self.y_train = tf.keras.utils.to_categorical(self.y_train, self.NUM_CLASSES)
+
+        test_df = pd.read_csv("sign_mnist_test.csv")
+        self.y_test = test_df['label']
+        del test_df['label']
+
+        self.x_test = test_df.values
+        self.x_test = self.x_test / 255
+        self.x_test = self.x_test.reshape(-1, 28, 28, 1)
+        self.y_test = tf.keras.utils.to_categorical(self.y_test, self.NUM_CLASSES)
+        
+        
+        self.model = SignLanguageClassifier._load_model()
   
-		# normalize the pixels
-		self.X_train = self.X_train.astype('float32') / 255 
-		self.X_test = self.X_test.astype('float32') / 255 
-		self.X_valid = self.X_valid.astype('float32') / 255
-		# one hot encoding of y train
-		self.y_train = self.data_train['letter'].astype('category').cat.codes
-		self.y_train = tf.keras.utils.to_categorical(self.y_train,self.NUM_CLASSES)
+
+    
+    def fit(self, folder):
+        """Fit the model using the data in the selected directory"""
   
-		# one hot encoding of y test
-		self.y_test = self.data_test['letter'].astype('category').cat.codes
-		self.y_test = tf.keras.utils.to_categorical(self.y_test, self.NUM_CLASSES)
-  
-		# one hot encoding of y valid
-		self.y_valid = self.data_valid['letter'].astype('category').cat.codes
-		self.y_valid = tf.keras.utils.to_categorical(self.y_valid, self.NUM_CLASSES)
-		
-		self.model = SignLanguageClassifier._load_model()
+        train_set,valid_set = self._gen_data()
+        
+        # callback object to save weights during the training
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(
+            filepath=os.path.join(SAVE_DIR, "weights-{epoch:03d}.ckpt"),
+            save_weights_only=True,
+            verbose=1,
+        )
 
-	def _read_csv_data(self, dir,data_csv):
-		# read and resize the images
-		X = np.array([cv2.resize(
-				cv2.imread(os.path.join(dir, img_path)),
-				(self.IMG_HEIGHT, self.IMG_WIDTH)) for img_path in data_csv["file_name"]])
-		return X
+        # Fit the model
+        history = self.model.fit(
+            train_set,
+            epochs=self.epochs,
+            callbacks=[cp_callback],
+            validation_data=valid_set,
+        )
 
-	def fit(self, folder):
-		"""Fit the model using the data in the selected directory"""
-  
-		# Split data into training+validation and testing sets
-		# X_train, X_test, y_train, y_test = train_test_split(
-		# 	self.X_train, self.y_train, random_state=42)
-		# X_train, X_val, y_train, y_val = train_test_split(
-		# 	X_train, y_train, random_state=42)
+        # Show the predictions on the testing set
+        result = self.model.evaluate(self.X_test,self.y_test, batch_size=self.BATCH_SIZE)
+        print(
+            "Testing set evaluation:",
+            dict(zip(self.model.metrics_names, result)),
+        )
 
-		# callback object to save weights during the training
-		cp_callback = tf.keras.callbacks.ModelCheckpoint(
-			filepath=os.path.join(SAVE_DIR, "weights-{epoch:03d}.ckpt"),
-			save_weights_only=True,
-			verbose=1,
-		)
+        # Save model information
+        self.model.save(folder)
 
-		# Fit the model
-		history = self.model.fit(
-			self.X_train,self.y_train,
-			epochs=self.epochs,
-			validation_data=(self.X_valid ,self.y_valid),
-			callbacks=[cp_callback],
-		)
+        # Plot training results
+        epochs_range = range(self.epochs)
 
-		# Show the predictions on the testing set
-		result = self.model.evaluate(self.X_test,self.y_test, batch_size=self.BATCH_SIZE)
-		print(
-			"Testing set evaluation:",
-			dict(zip(self.model.metrics_names, result)),
-		)
+        # Accuracy in training and validation sets as the training goes
+        acc = history.history["accuracy"]
+        val_acc = history.history["val_accuracy"]
+        plt.figure(figsize=(8, 4))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc, label="Training Accuracy")
+        plt.plot(epochs_range, val_acc, label="Validation Accuracy")
+        plt.legend(loc="lower right")
+        plt.title("Training and Validation Accuracy for DenseNet")
 
-		# Save model information
-		self.model.save(folder)
+        # Loss in training and validation sets as the training goes
+        loss = history.history["loss"]
+        val_loss = history.history["val_loss"]
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label="Training Loss")
+        plt.plot(epochs_range, val_loss, label="Validation Loss")
+        plt.legend(loc="upper right")
+        plt.title("Training and Validation Loss for DenseNet")
 
-		# Plot training results
-		epochs_range = range(self.epochs)
+        plt.savefig(os.path.join(SAVE_DIR, "results.png"))
 
-		# Accuracy in training and validation sets as the training goes
-		acc = history.history["accuracy"]
-		val_acc = history.history["val_accuracy"]
-		plt.figure(figsize=(8, 4))
-		plt.subplot(1, 2, 1)
-		plt.plot(epochs_range, acc, label="Training Accuracy")
-		plt.plot(epochs_range, val_acc, label="Validation Accuracy")
-		plt.legend(loc="lower right")
-		plt.title("Training and Validation Accuracy for DenseNet")
+    def _gen_data(self):
+        X_train, X_valid, y_train, y_valid = train_test_split(self.x_train, self.y_train)
+        
+        datagen = ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            # randomly rotate images in the range (degrees, 0 to 180)
+            rotation_range=10,
+            zoom_range=0.1,  # Randomly zoom image
+            # randomly shift images horizontally (fraction of total width)
+            width_shift_range=0.1,
+            # randomly shift images vertically (fraction of total height)
+            height_shift_range=0.1,
+            horizontal_flip=False,  # randomly flip images
+            vertical_flip=False  # randomly flip images
+           )  
+        train_generator = datagen.flow(X_train,y_train,batch_size=self.BATCH_SIZE)
+        valid_generator = datagen.flow(X_valid,y_valid,batch_size=self.BATCH_SIZE)
+        
+        return train_generator, valid_generator
 
-		# Loss in training and validation sets as the training goes
-		loss = history.history["loss"]
-		val_loss = history.history["val_loss"]
-		plt.subplot(1, 2, 2)
-		plt.plot(epochs_range, loss, label="Training Loss")
-		plt.plot(epochs_range, val_loss, label="Validation Loss")
-		plt.legend(loc="upper right")
-		plt.title("Training and Validation Loss for DenseNet")
 
-		plt.savefig(os.path.join(SAVE_DIR, "results.png"))
+    @classmethod
+    def _load_model(cls):
+        """Build a DenseNet model for image classification"""
+        model = DenseNet_Architecture.DenseNet(cls.INPUT_SHAPE,cls.NUM_CLASSES)
 
-	@classmethod
-	def _load_model(cls):
-		"""Build a DenseNet model for image classification"""
-		model = DenseNet_Architecture.DenseNet(cls.INPUT_SHAPE,cls.NUM_CLASSES)
+        model.compile(loss="categorical_crossentropy",
+                       optimizer="adam", metrics=['accuracy', 'AUC'])
 
-		model.compile(loss="categorical_crossentropy",
-		               optimizer="adam", metrics=['accuracy', 'AUC'])
+        # Print model summary
+        model.summary()
 
-		# Print model summary
-		model.summary()
-
-		return model
+        return model
 
 
 if __name__ == "__main__":
 
-	parser = argparse.ArgumentParser(
-		description="DenseNet Trainer for American Sign Language app.")
+    parser = argparse.ArgumentParser(
+        description="DenseNet Trainer for American Sign Language app.")
 
-	parser.add_argument(
-		"-f",
-		"--folder",
-		type=str,
-		help="Destination folder to save the model after training ends.",
-		default="Custom",
-	)
-	args = parser.parse_args()
+    parser.add_argument(
+        "-f",
+        "--folder",
+        type=str,
+        help="Destination folder to save the model after training ends.",
+        default="Custom",
+    )
+    args = parser.parse_args()
 
-	if Path(f"model_{args.folder}").is_dir():
-		print(
-			f"Folder model_{args.folder} already exists do you want to overwrite ?")
-		y = input('Type "Yes" or "No": ')
-		if y != "Yes":
-			print("Aborting.")
-			sys.exit()
+    if Path(f"model_{args.folder}").is_dir():
+        print(
+            f"Folder model_{args.folder} already exists do you want to overwrite ?")
+        y = input('Type "Yes" or "No": ')
+        if y != "Yes":
+            print("Aborting.")
+            sys.exit()
 
-	clf = SignLanguageClassifier()
-	clf.fit(Path(f"model_{args.folder}"))
+    clf = SignLanguageClassifier()
+    clf.fit(Path(f"model_{args.folder}"))
